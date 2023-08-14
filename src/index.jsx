48cab6976e401322587f78dd7c5004ab5e6ddafa
cmddev-admin <daly.caroline@live.com>
2023-08-13 19:11:26 -0500

    additional favouriting code

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// import MovieDetailsPage from './pages/movieDetailsPage';
-// import sample from './stories/sampleData';
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-
-// const movies = [sample, sample, sample, sample, sample, sample, sample];
-
-// const App = () => {
-//     return (
-//       <MovieDetailsPage movie={sample} images={images} />
-//     );
-//   };
-
-// const images = [
-//     "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
-//     "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
-//     "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
-//     "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
-//   ]
-
-// const rootElement = createRoot(document.getElementById("root"));
-// rootElement.render(<App />);
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies/favourites">Favourites</Link>
+        </li>
+      </ul>
       <Routes>
+        <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
